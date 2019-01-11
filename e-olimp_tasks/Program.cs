31cf4d4f772943bc1fd04c99f59f6de49e1bf004
Dmytriiev Oleksandr <Lvbnhbr199809@gmail.com>
2019-01-11 20:48:56 +0200

    Add project files.

---
+++ e-olimp_tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace e_olimp_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "";
+            number = Convert.ToInt32((Console.ReadLine())).ToString();
+            Console.WriteLine($"{number[0]} {number[1]}");
+        }
+    }
+}
